7c7fcf1d7911579ab0f56c0cb6a645a8479a4b20
Armen Agabekian <2008nv@gmail.com>
2024-07-25 17:22:38 -0700

    full-time-schedule-and-display-2

--- util.js
+++ util.js
@@ -6,9 +6,16 @@
 //     const formattedHours = hours % 12 || 12; // Convert hours to 12-hour format
 //     return `${formattedHours}:${minutes < 10 ? '0' : ''}${minutes} ${period}`;
 // };
+
 export const formatFullDateTime = (dateTime) => {
     const date = new Date(dateTime);
-    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
-    return date.toLocaleDateString('en-US', options);
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Get month and pad with zero if needed
+    const day = date.getDate().toString().padStart(2, '0'); // Get day and pad with zero if needed
+    const year = date.getFullYear().toString().slice(-2); // Get the last two digits of the year
+    const hours = date.getHours().toString().padStart(2, '0'); // Get hours and pad with zero if needed
+    const minutes = date.getMinutes().toString().padStart(2, '0'); // Get minutes and pad with zero if needed
+    const seconds = date.getSeconds().toString().padStart(2, '0'); // Get seconds and pad with zero if needed
+
+    return `${month}/${day}/${year} ${hours}:${minutes}`;
 };
-// Other utility functions if any
+
